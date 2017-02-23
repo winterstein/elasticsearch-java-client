9de651459b84e7965e2963b091a3e8855c845327
Daniel Winterstein <daniel@sodash.com>
2017-02-23 14:30:42 +0000

    sync

--- src/com/winterwell/es/client/IndexRequestBuilder.java
+++ src/com/winterwell/es/client/IndexRequestBuilder.java
@@ -1,5 +1,6 @@
 package com.winterwell.es.client;
 
+import java.util.Arrays;
 
 /**
  * Index (store) a document.
@@ -14,6 +15,25 @@ public class IndexRequestBuilder extends ESHttpRequest<IndexRequestBuilder, IESR
 		super(esHttpClient);
 		method = "POST";
 		bulkOpName = "index";
+	}
+
+	/**
+	 * Force a refresh?
+	 * See https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-refresh.html
+	 * @param string false | true | wait_for
+	 */
+	public void setRefresh(String refresh) {
+		assert Arrays.asList("false","true","wait_for").contains(refresh) : refresh;
+		params.put("refresh", refresh);		
+	}
+
+	/**
+	 * If true, this will fail if a document with this ID already exists.
+	 * @param createOnly
+	 */
+	public void setOpTypeCreate(boolean createOnly) {
+		if (createOnly) params.put("op_type", "create");
+		else params.remove("op_type");
 	}
 
 }
