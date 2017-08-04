b85beecd2435184ea8ba139551b4b1ebe05454e8
Daniel Winterstein <daniel@sodash.com>
2017-08-04 12:38:48 +0100

    ESQB

---
+++ src/com/winterwell/es/client/query/ESQueryBuilders.java
@@ -0,0 +1,14 @@
+package com.winterwell.es.client.query;
+
+import java.util.Map;
+
+import com.winterwell.utils.TodoException;
+
+public class ESQueryBuilders {
+
+	public static Map queryStringQuery(String q) {
+		// TODO Auto-generated method stub
+		throw new TodoException();
+	}
+
+}
