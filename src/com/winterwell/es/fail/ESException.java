9b4fd7ce93f6b7d316bc7ff4b04adf004cf7029c
Daniel Winterstein <daniel@sodash.com>
2017-10-01 08:51:30 +0100

    sync

--- src/com/winterwell/es/fail/ESException.java
+++ src/com/winterwell/es/fail/ESException.java
@@ -1,12 +1,16 @@
 package com.winterwell.es.fail;
 
+import com.winterwell.utils.WrappedException;
+
 /**
  * Wrap e.g. IOExceptions, so that the higher-up code can catch and filter
  * that these are ES exceptions.
+ * 
+ * TODO in multi-thread usage, ES client logs the caller, so can this report the caller stack??
  * @author daniel
  *
  */
-public class ESException extends RuntimeException {
+public class ESException extends WrappedException {
 
 	public ESException(String msg, Throwable ex) {
 		super(msg, ex);
