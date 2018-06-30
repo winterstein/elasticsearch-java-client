2f3bb8dda26d87ad06fe24166e1ae0eb671aa6ea
Daniel Winterstein <daniel@sodash.com>
2018-06-30 18:04:11 +0100

    painless Maps are actually painful

--- src/com/winterwell/es/client/PainlessScriptBuilder.java
+++ src/com/winterwell/es/client/PainlessScriptBuilder.java
@@ -4,6 +4,8 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.lucene.index.MergeRateLimiter;
+
 import com.winterwell.utils.StrUtils;
 import com.winterwell.utils.Utils;
 import com.winterwell.utils.containers.ArrayMap;
@@ -34,53 +36,65 @@ public class PainlessScriptBuilder {
 		return script;
 	}
 
+	/**
+	 * Make an update script for this doc.
+	 * @param doc
+	 * @return this
+	 */
 	public static PainlessScriptBuilder fromJsonObject(Map<String, Object> doc) {
+		// TODO maybe refactor to use Merger and Diff from Depot??
 		// NB use Debug.explain(var) to get debug info out
 		PainlessScriptBuilder psb = new PainlessScriptBuilder();
 		StringBuilder sb = new StringBuilder("Map e=ctx._source;\n");
 		String var = "e";
+		fromJsonObject2(doc, psb, sb, var);
+		psb.script = sb.toString();
+		return psb;
+	}
+
+	private static void fromJsonObject2(Map<String, Object> doc, PainlessScriptBuilder psb, StringBuilder sb, String var) {
 		for(Map.Entry me : doc.entrySet()) {
 			Object v = me.getValue();
-			if (v==null) continue;			
+			if (v==null) continue;
+			
+			// collections?
+			if (v instanceof Collection || v.getClass().isArray()) {
+				String el = var+"."+me.getKey();
+				List<Object> vlist = Containers.asList(v);
+				// shove into params
+				String pid = psb.addParam(vlist);								
+				// ??uniqueness?? Left for the user -- get the last value				
+				sb.append("if ("+el+"!=null) "+el+".addAll(params."+pid+"); else "+el+"=params."+pid+";\n");
+				continue;
+			} else if (v instanceof Map) {
+				String el = var+"."+me.getKey();
+				Map vmap = (Map) v;								
+				// shove into params
+				String pid = psb.addParam(vmap);
+				// TODO recurse??
+				sb.append("if ("+el+"==null) "+el+"=params."+pid+"; else "+el+".putAll(params."+pid+");\n");
+				continue;
+			}
+			
 			String vs;
 			if (v instanceof String) {
 				vs = StrUtils.convertToJavaString((String) v);
 			} else if (v instanceof Time) {
 				vs = '"'+((Time)v).toISOString()+'"'; 
-			} else if (v instanceof Collection || v.getClass().isArray()) {
-				String el = var+"."+me.getKey();
-				List<Object> vlist = Containers.asList(v);
-				
-//				// HACK special case props!!!
-//				if ("props".equals(me.getKey())) {
-//					sb.append("if ("+el+"==null) "+el+" = new ArrayList();\n");
-//					for (Object _kv : vlist) {
-//						Map kv = (Map) _kv;
-//						String k = (String) kv.get("k");
-//						String pid = psb.addParam(kv);
-//						sb.append("if () el.add(params."+pid+")");
-//						List x;						
-//					}					
-//					continue;
-//				}
-				
-				String pid = psb.addParam(vlist);				
-				
-				// TODO uniqueness??
-				// Left for the user -- get the last value
-				
-				sb.append("if ("+el+"!=null) "+el+".addAll(params."+pid+"); else "+el+"=params."+pid+";\n");
-				continue;
 			} else if (v instanceof XId) {
 				vs = StrUtils.convertToJavaString(v.toString());
 			} else {
 				// just number, boolean I think??
 				vs = v.toString();
 			}
-			sb.append(var+"."+me.getKey()+" = "+vs+";\n");
+			String k = (String) me.getKey();
+			if (StrUtils.isWord(k)) {
+				sb.append(var+"."+k+" = "+vs+";\n");
+			} else {
+				// handle e.g. "@class"
+				sb.append(var+".put(\""+k+"\","+vs+");\n");
+			}
 		}	
-		psb.script = sb.toString();
-		return psb;
 	}
 
 	private String addParam(Object paramValue) {
