2eb4ac13096c8ecdc67cf8392a3074f7dc944838
Daniel Winterstein <daniel@sodash.com>
2018-08-14 15:25:40 +0100

    sync

--- src/com/winterwell/es/ESKeyword.java
+++ src/com/winterwell/es/ESKeyword.java
@@ -6,6 +6,11 @@ import java.lang.annotation.RetentionPolicy;
 /**
  * A field marker for "ElasticSearch should treat this as a keyword (not text)"
  * - since that is the most common schema error. 
+ * 
+ * Usage: This will NOT do anything by itself!
+ * You'll need your own reflection code to spot annotations and make ES mappings.
+ * TODO refactor AppUtils mapping-maker into a generic class here.
+ * 
  * @author daniel
  *
  */
