ed2ea55bc1e682e8ca5f73943b62d793b2ca1108
Daniel Winterstein <daniel@sodash.com>
2018-08-14 22:20:30 +0100

    sync

---
+++ src/com/winterwell/es/client/ESJC.java
@@ -0,0 +1,12 @@
+package com.winterwell.es.client;
+
+/**
+ * A shorthand convenience/marketing name for {@link ESHttpClient}
+ * Welcome to ElasticSearch Java Client by Good-Loop!
+ * 
+ * @author daniel
+ *
+ */
+public final class ESJC extends ESHttpClient {
+
+}
