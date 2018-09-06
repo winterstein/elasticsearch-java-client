5e7822b9427d84774e030e0d90b9cedbf9a50473
Daniel Winterstein <daniel@winterwell.com>
2018-09-06 10:37:02 +0100

    ESJC-upgrade

---
+++ src/com/winterwell/es/ESKeyword.java
@@ -0,0 +1,21 @@
+package com.winterwell.es;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * A field marker for "ElasticSearch should treat this as a keyword (not text)"
+ * - since that is the most common schema error. 
+ * 
+ * Usage: This will NOT do anything by itself!
+ * You'll need reflection code to spot annotations and make ES mappings.
+ * AppUtils.initESMappings() does this.
+ * TODO refactor AppUtils mapping-maker into a generic class here.
+ * 
+ * @author daniel
+ *
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ESKeyword {
+
+}
