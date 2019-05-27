e0f589e37c46e22a44650755ed06f1b0de371bae
DW-Stross <daniel@good-loop.com>
2019-05-27 16:44:06 +0100

    hopefully more robust ES merge

--- src/com/winterwell/es/client/update.painless.js
+++ src/com/winterwell/es/client/update.painless.js
@@ -10,13 +10,23 @@ for(def kv : p0.entrySet()) {
 	} else {
 // assume: old-new type does not change
 		if (v instanceof Map) {
+			// merge in Map key-values
 			old.putAll(v);
 		} else if (v instanceof List) {
-			for(int i=0; i<v.size(); i++) {
-				def x=v.get(i); 
-				if ( ! old.contains(x)) old.add(x);
+			// Handle the case where old isnt a List (see lg bugs May 2019)
+			if ( ! old instanceof Collection) {
+				def v2 = new ArrayList();
+				v2.add(old); v2.addAll(v);
+				e.put(k, v2);
+			} else {
+				// merge in new List values
+				for(int i=0; i<v.size(); i++) {
+					def x=v.get(i);
+					if ( ! old.contains(x)) old.add(x);				
+				}
 			}
 		} else {
+			// just overwrite
 			e.put(k, v);
 		}
 	}
